f7ac4ad91fb08f9e22e15ab8f8c08c139591c324
Jonas Bak Phillipson <jbakchr@gmail.com>
2026-06-22 08:46:52 +0200

    feat(config): include container_port in prompts for postgres

--- src/dx/config/images.py
+++ src/dx/config/images.py
@@ -7,7 +7,7 @@ IMAGE_PROFILES = {
     "postgres": {
         "container_port": 5432,
         "default_host_port": 5432,
-        "prompts": ["port", "env"],
+        "prompts": ["port", "container_port", "env"],
         "env": {
             "POSTGRES_PASSWORD": "password",
         },
@@ -15,12 +15,12 @@ IMAGE_PROFILES = {
     "redis": {
         "container_port": 6379,
         "default_host_port": 6379,
-        "prompts": ["port"],
+        "prompts": ["port", "container_port"],
     },
     "mysql": {
         "container_port": 3306,
         "default_host_port": 3306,
-        "prompts": ["port", "env"],
+        "prompts": ["port", "container_port", "env"],
         "env": {
             "MYSQL_ROOT_PASSWORD": "password",
         },
