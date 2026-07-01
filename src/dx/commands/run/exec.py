dd4930cfdf9dc6d8e57687dd5b81341afb6221e6
Jonas Bak Phillipson <jbakchr@gmail.com>
2026-07-01 16:07:07 +0200

    refactor(exec): add docstring

--- src/dx/commands/run/exec.py
+++ src/dx/commands/run/exec.py
@@ -2,7 +2,20 @@ import subprocess
 
 from dx.ui.output import execution_header, execution_done
 
-def execute(cmd: str):
+
+def execute(cmd: str) -> None:
+    """
+    Execute a Docker command.
+
+    Displays execution status messages before and after
+    running the command.
+
+    Args:
+        cmd: The Docker command to execute.
+
+    Returns:
+        None.
+    """
     execution_header()
     subprocess.run(cmd, shell=True)
     execution_done()
